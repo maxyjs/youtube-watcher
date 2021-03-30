2d9b804c48904b25b25ff0534b4b19cef4a8988c
SokolovDADeveloper <sokolovdadeveloper@gmail.com>
2021-03-30 12:21:38 +0300

    add scraping for various options sorting results

--- resultsHandlers/getResultsForTerm.js
+++ resultsHandlers/getResultsForTerm.js
@@ -1,50 +1,131 @@
-const scraper = require('../scrapers/scraper')
+const scraper = require('../scrapers/scraper');
+const { showErrorResultScrapeSerp } = require('./../appSettings');
 
-function createUrlFromOptions(queryOptions, numPage = 1) {
+function createUrlsByDateTime(chunk, queryOptions) {
+  const urls = [];
 
-  // thisMonth: "&sp=EgIIBA%253D%253D",
-  // allTime: "&sp=CAISAhAB"  <--- videos by upload date
-  // allTime: "&sp=EgIQAQ%253D%253D"  <--- video by relevance
+  const indexTimes = {
+    thisMonth: [
+      '&sp=EgIIBA%253D%253D', // by relevance
+      '&sp=CAISBAgEEAE%253D', // by date upload
+      '&sp=CAMSBAgEEAE%253D', // by views
+      '&sp=CAESBAgEEAE%253D', // by rating
+    ],
+    thisYear: [
+      '&sp=CAASBAgFEAE%253D', // by relevance
+      '&sp=CAISBAgFEAE%253D ', // by date upload
+      '&sp=CAMSBAgFEAE%253D', // by views
+      '&sp=CAESBAgFEAE%253D', // by rating
+    ],
+    allTime: [
+      '&sp=EgIQAQ%253D%253D', // by relevance
+      '&sp=CAISAhAB', // by date upload
+      '&sp=CAMSAhAB', // by views
+      '&sp=CAESAhAB', // by rating
+    ],
+  };
 
-  const enumDateTimeUpload = {
-    thisMonth: "&sp=EgIIBA%253D%253D",
-    // allTime: "&sp=CAISAhAB"
-    allTime: "&sp=EgIQAQ%253D%253D"  // allTime: "&sp=EgIQAQ%253D%253D"  <--- video by relevance
+  const { termOptions = {} } = queryOptions;
+
+  const whatTimes = termOptions.dateTimeUpload
+    ? termOptions.dateTimeUpload
+    : ['allTime', 'thisMonth', 'thisYear'];
+
+  const urlsByAllTimes = allTimes(chunk, indexTimes, whatTimes);
+
+  urls.push(...urlsByAllTimes);
+
+  return urls;
+
+  function allTimes(chunk, indexTimes, whatTimes) {
+    const setUrls = [];
+
+    for (const whatTime of whatTimes) {
+      const times = indexTimes[whatTime];
+      const urls = allSorted(chunk, times);
+      setUrls.push(...urls);
+    }
+
+    return setUrls;
   }
 
-  const defaultDateTimeUpload = "allTime"
+  function allSorted(chunk, times) {
+    const urls = times.map((time) => {
+      return `${chunk}${time}`;
+    });
+    return urls;
+  }
+}
 
-  const {term, termOptions = {}} = queryOptions
-
-  const dateTimeUpload = termOptions.dateTimeUpload ? termOptions.dateTimeUpload : defaultDateTimeUpload
-
-  const wordsWithIntitleCommand = modifyTermForSearchInTitleOnly(term)
-
-  const encodeTerm = encodeURIComponent(wordsWithIntitleCommand)
-  const url = `https://www.youtube.com/results?q=${encodeTerm}&page=${numPage}${enumDateTimeUpload[dateTimeUpload]}`
-  return url
+function prepareUrlChunk(term) {
+  const encodeTerm = encodeURIComponent(term);
+  const chunk = `https://www.youtube.com/results?q=${encodeTerm}`;
+  return chunk;
 }
 
 function modifyTermForSearchInTitleOnly(term) {
-  const words = term.split(' ')
-  const wordsWithIntitleCommand = words.map(word => {
-    return `intitle:${word}`
-  }).join(' ')
-  return wordsWithIntitleCommand
+  const words = term.split(' ');
+  const wordsWithIntitleCommand = words
+    .map((word) => {
+      return `intitle:${word}`;
+    })
+    .join(' ');
+  return wordsWithIntitleCommand;
+}
+
+function createUrls(queryOptions) {
+  const { term } = queryOptions;
+  const chunk = prepareUrlChunk(term);
+  const urlInTitleKeywords = prepareUrlChunk(
+    modifyTermForSearchInTitleOnly(term)
+  );
+
+  const urls = createUrlsByDateTime(chunk, queryOptions);
+  urls.push(urlInTitleKeywords);
+
+  return urls;
+}
+
+async function getResultsForAllUrls(queryOptions) {
+  let allResults = [];
+  const urls = createUrls(queryOptions);
+
+  for (const url of urls) {
+
+    let resultsForUrl = []
+
+    try {
+      const results = await scraper.scrapeResultSearchPage(url);
+      allResults.push(...results)
+
+      if (results.length >= 20) {
+        try {
+          const resultsPage2 = await scraper.scrapeResultSearchPage(`${url}&page=2`);
+          allResults.push(...resultsPage2);
+        } catch {
+          showErrorResultScrapeSerp &&
+          console.log(
+            '\x1b[33m%s\x1b[0m',
+            'PAGE 2: failed scrape url:\n',
+            `${url}&page=2`
+          ); // font yellow
+        }
+      }
+    } catch {
+      showErrorResultScrapeSerp &&
+      console.log('\x1b[33m%s\x1b[0m', 'failed scrape url:\n', url); // font yellow
+    }
+
+    allResults.push(...resultsForUrl)
+  }
+
+  return allResults;
 }
 
 async function getResultsForTerm(queryOptions) {
+  const resultsForAllUrls = getResultsForAllUrls(queryOptions);
 
-  const url = createUrlFromOptions(queryOptions)
-
-  let results = []
-
-  try {
-    results = await scraper.scrapeResultSearchPage(url)
-    return results
-  } catch {
-    return results
-  }
+  return resultsForAllUrls;
 }
 
 module.exports = getResultsForTerm;
