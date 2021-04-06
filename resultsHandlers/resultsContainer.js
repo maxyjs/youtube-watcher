d4e0c1a24356476c9f073c0b3695fe436ce9e788
SokolovDADeveloper <sokolovdadeveloper@gmail.com>
2021-04-06 18:14:01 +0300

    refactor resultsContainer

--- resultsHandlers/resultsContainer.js
+++ resultsHandlers/resultsContainer.js
@@ -1,25 +1,19 @@
-function addResult(result) {
-  const isHas = this.addedIds.has(result.video.id);
-  if (!isHas) {
-    this.results.push(result);
-    this.addedIds.add(result.video.id);
-  }
-}
-
-function getResults() {
-  return this.results;
-}
-
 const resultsContainer = {
   results: [],
   addedIds: new Set(),
-  addResult,
-  getResults,
-  resultsForEach(fn) {
-    for (let i = 0; i < this.results.length; i++){
-      fn(results[i])
+
+  addResult(result) {
+    const isHas = this.addedIds.has(result.video.id);
+    if (!isHas) {
+      this.results.push(result);
+      this.addedIds.add(result.video.id);
     }
-  }
+  },
+
+  getResults() {
+    return this.results;
+  },
 };
 
 module.exports = resultsContainer;
+
