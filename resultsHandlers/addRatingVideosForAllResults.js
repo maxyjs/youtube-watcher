0fcc34b275877e2bc0cbebcfc732e7e3052e9067
SokolovDADeveloper <sokolovdadeveloper@gmail.com>
2021-04-17 21:54:14 +0300

    ref(addRatingVideosForAllResults): refactoring async results iterator

--- resultsHandlers/addRatingVideosForAllResults.js
+++ resultsHandlers/addRatingVideosForAllResults.js
@@ -1,19 +1,10 @@
 const getRatingVideo = require('../scrapers/getRatingVideo.js');
 
-async function* asyncIterator(results) {
-  //TODO: Change iterator RATING
-  let i = -1;
-  while (i < results.length - 1) {
-    i++;
-    yield results[i];
-  }
-}
-
 async function addRatingVideosForAllResults(results) {
-  for await (const videoObj of asyncIterator(results)) {
+  for (const result of results) {
     try {
-      const rating = await getRatingVideo(videoObj.video.id);
-      videoObj.rating = rating;
+      const rating = await getRatingVideo(result.video.id);
+      result.rating = rating;
     } catch (err) {
       console.log(err);
     }
