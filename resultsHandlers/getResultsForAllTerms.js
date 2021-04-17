55e08be99ccfb8e994be57a567ddac4f009b46e8
SokolovDADeveloper <sokolovdadeveloper@gmail.com>
2021-04-17 22:01:32 +0300

    ref(getResultsForAllTerms): refactoring listQueries iterator

--- resultsHandlers/getResultsForAllTerms.js
+++ resultsHandlers/getResultsForAllTerms.js
@@ -2,16 +2,7 @@ const getResultsForTerm = require('./getResultsForTerm.js');
 const resultsContainer = require('./../resultsHandlers/resultsContainer');
 
 async function getResultsForAllTerms(listQueries) {
-  // todo: Change getResultsForAllTerms iterator
-  async function* asyncIterator(listQueries) {
-    let i = -1;
-    while (i < listQueries.length - 1) {
-      i++;
-      yield [listQueries[i], i];
-    }
-  }
-
-  for await (const [listQuery, i] of asyncIterator(listQueries)) {
+  for (const listQuery of listQueries) {
     try {
       await getResultsForTerm(resultsContainer, listQuery);
     } catch (err) {
